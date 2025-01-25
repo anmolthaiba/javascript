c1c5d7f396b5c38ef40a3174020fde1c15a3ff8f
Anmol Thaiba <thaibaanmol@gmail.com>
2025-01-25 04:03:33 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,18 @@
+"use strict" // this is for to use newer model of js
+
+//alert(10+10) // this is not possible in js
+
+let age = 18
+// datatype
+
+/* number
+bigint
+string
+boolean
+null
+undefined
+*/
+
+//object
+
+console.log(typeof age)
